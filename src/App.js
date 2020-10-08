36f369c7067ccb77eea44f7fe689fdb75285717a
Furuikeya <furuburi@gmail.com>
2020-10-08 20:55:24 +0300

    initial project version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import 'tachyons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      joke: '',
+      setup: '',
+      delivery: '',
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://sv443.net/jokeapi/v2/joke/Programming')
+    .then(response => response.json())
+    .then(result => {
+      if (result.type === 'single') {
+        return this.setState({ joke: result.joke })
+      } else {
+        return (
+          this.setState({ setup: result.setup }),
+          this.setState({ delivery: result.delivery })
+        )
+      }
+      
+    });
+  };
+
+  render() {
+    const {joke, setup, delivery} = this.state;
+    return (
+      <div className='tc'>
+        <button className='joke-button bg-gold bw3 br1 b--dark-red grow grow:hover '>joke</button>
+        <div className='joke f3 bg-gold'>
+          <div>{joke}</div>
+          <div>{setup}</div>
+          <div>{delivery}</div>  
+        </div>
+        
+      </div>
+      )
+  }
 }
 
 export default App;
