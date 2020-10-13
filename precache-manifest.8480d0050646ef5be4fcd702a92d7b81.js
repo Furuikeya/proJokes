68a209eb131b5e17ce72262d4ebfcaa10e40824d
Furuikeya <furuburi@gmail.com>
2020-10-13 20:09:03 +0300

    Updates

---
+++ precache-manifest.8480d0050646ef5be4fcd702a92d7b81.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c473da66d0bdeed7781e1db04e51985",
+    "url": "/projokes/index.html"
+  },
+  {
+    "revision": "59c6df0df93dae0d6a51",
+    "url": "/projokes/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "371bc713802d52197347",
+    "url": "/projokes/static/css/main.ea24c7e1.chunk.css"
+  },
+  {
+    "revision": "59c6df0df93dae0d6a51",
+    "url": "/projokes/static/js/2.979ff2dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projokes/static/js/2.979ff2dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "371bc713802d52197347",
+    "url": "/projokes/static/js/main.be4e06b6.chunk.js"
+  },
+  {
+    "revision": "935337e13304b9fdf69e",
+    "url": "/projokes/static/js/runtime-main.89671592.js"
+  },
+  {
+    "revision": "567860c5074f320995d5c58e1b4a3ec7",
+    "url": "/projokes/static/media/bitwise.567860c5.ttf"
+  }
+]);
